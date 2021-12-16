f10753f0147ece1ae047220b01c451030b2f5b5f
Mallikarjun Jamadarkhani <54496975+Mallikarjun29@users.noreply.github.com>
2021-12-16 13:39:19 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    output='Nothing'
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    if(prediction[0]==0):
+        output='Polymer'
+    elif(prediction[0]==1):
+        output='Metal'
+    else:
+        output='Ceramic'
+
+    return render_template('index.html', prediction_text='The material is {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
