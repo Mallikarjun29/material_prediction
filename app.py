11520258219d4d8dd436f5baa1272dc48ffb75ad
Mallikarjun Jamadarkhani <54496975+Mallikarjun29@users.noreply.github.com>
2021-12-16 21:27:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-    if(prediction[2]==0):
+    if(prediction[1]==2):
         output='Polymer'
     elif(prediction[1]==1):
         output='Metal'
-    elif(prediction[0]==1):
+    elif(prediction[1]==0):
         output='Ceramic'
 
     return render_template('index.html', prediction_text='The material is {}'.format(output))
